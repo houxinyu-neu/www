151c3c2a2017428b59e2af3407119d2b9fe8a85c
houxinyu-neu <1119676501@qq.com>
2022-12-06 19:45:21 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,95 @@
+/**
+ * Created by bmy on 16-11-18.
+ */
+
+// 其他数据
+var WebConfig = {
+        searchbar: 'Cancel',
+        placeholder: 'Enter a keyword and press Enter to search!',
+        hotimg:'../img/hot.png',
+       hottitle: 'Dont carry the bags on the street again, these bags are popular this year!'
+    }
+
+// 轮播图数据
+var Carousel = [
+    {
+        Carouselimg:'../img/banner3.png'
+    },
+    {
+        Carouselimg:'../img/banner4.jpg'
+    },
+    {
+        Carouselimg:'../img/banner5.jpg'
+    }
+]
+
+// 分类栏目数据
+var ClassNames = [
+    {
+        imgsrc: '../img/tianmao.png',
+        name: 'Tmall'
+    },
+    {
+        imgsrc: '../img/juhsuan.png',
+        name: 'Poly'
+    },
+    {
+        imgsrc: '../img/jingkou.png',
+        name: 'Tmall Global'
+    },
+    {
+        imgsrc: '../img/waimai.png',
+        name: 'takeout'
+    },
+    {
+        imgsrc: '../img/chaoshi.png',
+        name: 'upermarket'
+    },
+    {
+        imgsrc: '../img/chong.png',
+        name: 'recharge'
+    },
+    {
+        imgsrc: '../img/lvxing.png',
+        name: 'travel'
+    },
+    {
+        imgsrc: '../img/taobao.png',
+        name: 'gold coins'
+    },
+    {
+        imgsrc: '../img/home.png',
+        name: 'home'
+    },
+    {
+        imgsrc: '../img/more.png',
+        name: 'Classification'
+    },
+
+];
+
+// 主商品数据
+var ShopInfo= [
+    {
+        shoptitle:'smart life',
+        shopcontent:'Mac Book',
+        shopimg: '../img/mac.jpeg'
+    },
+    {
+        shoptitle:'Food City',
+        shopcontent:'3 pieces 40% off',
+        shopimg: '../img/food.jpeg'
+    },
+    {
+        shoptitle:'Household',
+        shopcontent:'Dishwasher Festival',
+        shopimg: '../img/home.jpeg'
+    },
+    {
+        shoptitle:'luxury',
+        shopcontent:'000-150',
+        shopimg: '../img/lurxy.png'
+    },
+]
+
+
